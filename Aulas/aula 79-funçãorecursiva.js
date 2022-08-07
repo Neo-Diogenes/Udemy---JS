3a17d17e6433be2c16bf7d5a10dee88b73b5e96b
Neo-Diogenes <klebs123.ss@gmail.com>
2022-08-07 20:23:34 -0300

    novo

---
+++ Aulas/aula 79-funçãorecursiva.js
@@ -0,0 +1,15 @@
+
+function recursiva(max){
+
+    if (max >= 10) return;
+    max++;
+console.log(max)
+    recursiva(max);
+
+}
+
+
+
+recursiva(1)
+
+//usado pra loop
---
+++ Aulas/aula79.funcoesgeradoras.js
@@ -0,0 +1,16 @@
+
+
+function* geradora1(){
+    // código qualquer
+
+    yield 'valor 1'
+
+     // código qualquer
+    
+     yield 'valor 2'
+
+      // código qualquer
+    
+    yield 'valor 3'
+
+}
